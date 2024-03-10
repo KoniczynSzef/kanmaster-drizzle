3ae6e1a0e80b3a7155df062eddcb71843bf79cb8
Koniczyn <piotr12konczyk27@gmail.com>
2024-03-10 11:23:29 +0100

    chore: added eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import React from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Edit</h1>      
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    );
 }
